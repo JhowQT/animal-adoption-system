635fae36d80c73d2f88eb5a82b4ccf2bc1ad524a
JhowQT <jhon.q.torrez@gmail.com>
2026-04-22 13:40:42 -0300

    ajustes

--- animal-service/src/main/java/com/cp5/animalservice/client/RestTemplateConfig.java
+++ animal-service/src/main/java/com/cp5/animalservice/client/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.cp5.animalservice.config;
+package com.cp5.animalservice.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
