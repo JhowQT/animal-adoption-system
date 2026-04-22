812ba3c4c97f02dd6ffa34850c5040f00ea2aa0c
JhowQT <jhon.q.torrez@gmail.com>
2026-04-21 22:07:04 -0300

    Adoption model

---
+++ adoption-service/src/test/java/com/cp5/adoptionservice/AdoptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp5.adoptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
